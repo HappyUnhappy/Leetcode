d4ab76014703fa53bd51f1523099a4ff5ffc5694
Petukhova Tatyana <t.petukhova@gpsolutions.com>
2021-10-31 23:21:06 +0300

    Added new class: RemoveDuplicatesFromSortedArray

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,8 +1,20 @@
 package com.leetcode;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        int[] nums = {0, 0, 1, 1, 1, 2};
+        int[] nums2 = {0, 0, 1, 1, 1};
+        int[] nums3 = {1, 1, 2};
+
+	    RemoveDuplicatesFromSortedArray object = new RemoveDuplicatesFromSortedArray();
+        int result = object.removeDuplicates(nums);
+        int result2 = object.removeDuplicates(nums2);
+        int result3 = object.removeDuplicates(nums3);
+
+        System.out.println("Test 1. Result = " + result + " and nums = " + Arrays.toString(nums));
+        System.out.println("Test 2. Result = " + result2 + " and nums = " + Arrays.toString(nums2));
+        System.out.println("Test 3. Result = " + result3 + " and nums = " + Arrays.toString(nums3));
     }
 }
--- src/com/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/com/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -2,6 +2,30 @@ package com.leetcode;
 
 public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
+        int inputArrayLength = nums.length;
 
+        if (inputArrayLength <= 1) {
+            return inputArrayLength;
+        }
+
+        int uniqueArrayNumbersCount = 1;
+        int previousNumber = nums[0];
+        int switchNumberIndex = -1;
+
+        for (int i = 1; i < inputArrayLength; ++i) {
+            if (nums[i] != previousNumber) {
+                ++uniqueArrayNumbersCount;
+                previousNumber = nums[i];
+
+                if(switchNumberIndex != -1) {
+                    nums[switchNumberIndex] = nums[i];
+                    ++switchNumberIndex;
+                }
+            } else if (switchNumberIndex == -1){
+                switchNumberIndex = i;
+            }
+        }
+
+        return uniqueArrayNumbersCount;
     }
 }
